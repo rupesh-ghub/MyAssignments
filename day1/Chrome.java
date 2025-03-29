3a538518438c7892c987635e10db11744dba1544
rupesh-ghub <rupesh06@gmail.com>
2025-03-29 13:30:21 -0500

    Add files via upload
    Day1

---
+++ day1/Chrome.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		float version=91.0f;
+		String developer="Google";
+		Boolean isBeta=false;
+		int releaseYear=2008;
+		char shortcutKey='C';
+		System.out.println("Version is: " +version);
+		System.out.println("Developer is: " +developer);
+		System.out.println("Is Beta: " +isBeta);
+		System.out.println("Release Year: " +releaseYear);
+		System.out.println("Shortcut Key is: " +shortcutKey);	
+
+	}
+
+}
