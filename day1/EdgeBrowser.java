009a575765fb96db84322c7c9c38422a7c1a8b6d
rupesh-ghub <rupesh06@gmail.com>
2025-04-05 17:51:42 -0500

    Add files via upload

---
+++ day1/EdgeBrowser.java
@@ -0,0 +1,12 @@
+package week2.day1;
+
+public class EdgeBrowser {
+
+	public static void main(String[] args) {
+		Browser allUrlTest = new Browser();
+		allUrlTest.launchBrowser("Browser Launched Successfully");
+		allUrlTest.loadUrl();
+
+	}
+
+}
